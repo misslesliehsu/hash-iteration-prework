e63a41bff346cf725795c7a609c94fccccee87b4
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-13 19:05:45 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,17 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+    passengers.each do |suite, name|
+      if suite == :suite_a
+        if name[0] == "A"
+          return name
+        end
+      end
+    end
 end
